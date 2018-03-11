7d1767d3ef701f7497f8b3fff72bfde08d9cd01c
Tyler Tian <tylertian123@gmail.com>
2018-03-11 13:25:19 -0400

    Tidy things up

--- src/main/DigitRecognitionBasic.java
+++ src/main/DigitRecognitionBasic.java
@@ -13,29 +13,24 @@ public class DigitRecognitionBasic {
 
 	public static void main(String[] args) {
 		try {
-			MNISTImage[] trainingData = MNISTLoader.loadTrainingImages();
-			//MNISTImage[] expandedTrainingData = MNISTLoader.loadImagesFromFile(new File("data\\expanded_training_images"), new File("data\\expanded_training_labels"));
-			MNISTImage[] evalData = MNISTLoader.loadTestingImages();
-			MNISTImage[] validationData = MNISTLoader.loadValidationImages();
+			MNISTImage[] trainingImages = MNISTLoader.loadTrainingImages();
+			MNISTImage[] testingImages = MNISTLoader.loadTestingImages();
+			MNISTImage[] expanded1 = MNISTLoader.loadImagesFromFile(new File("data\\expanded_training_images_1"), new File("data\\expanded_training_labels_1"));
+			MNISTImage[] expanded2 = MNISTLoader.loadImagesFromFile(new File("data\\expanded_training_images_2"), new File("data\\expanded_training_labels_2"));
 			
-			/*MNISTImage[] expandedData = new MNISTImage[trainingData.length + expandedTrainingData.length];
-			System.arraycopy(trainingData, 0, expandedData, 0, trainingData.length);
-			System.arraycopy(expandedTrainingData, 0, expandedData, trainingData.length, expandedTrainingData.length);
+			MNISTImage[] expandedImages = new MNISTImage[trainingImages.length + expanded1.length + expanded2.length];
+			System.arraycopy(trainingImages, 0, expandedImages, 0, trainingImages.length);
+			System.arraycopy(expanded1, 0, expandedImages, trainingImages.length, expanded1.length);
+			System.arraycopy(expanded2, 0, expandedImages, trainingImages.length + expanded1.length, expanded2.length);
 			
-			ClassificationNeuralNetwork<MNISTImage> net = 
-					new ClassificationNeuralNetwork<MNISTImage>(new int[] {MNISTImage.PIXEL_COUNT, 100, 10}, 
-							ClassificationNeuralNetwork.SIGMOID_ACTIVATION,
-							ClassificationNeuralNetwork.CROSSENTROPY_SIGMOID_COST);
-			net.SGDScheduledEta(expandedData, 1, 0.06, 5.0, evalData, 4, 0.5, 5);
-			net.saveData(new File("expandedresult.ann"));*/
-			MNISTImage[] expandedTrainingData = DatabaseExpander.expandByTranslation(trainingData, 2);
-			MNISTImage[] pt1 = new MNISTImage[expandedTrainingData.length / 2];
-			MNISTImage[] pt2 = new MNISTImage[expandedTrainingData.length - pt1.length];
-			System.arraycopy(expandedTrainingData, 0, pt1, 0, pt1.length);
-			System.arraycopy(expandedTrainingData, expandedTrainingData.length / 2, pt2, 0, pt2.length);
-			MNISTLoader.saveImages(pt1, new File("data\\expanded_training_images_1"), new File("data\\expanded_training_labels_1"));
-			MNISTLoader.saveImages(pt2, new File("data\\expanded_training_images_2"), new File("data\\expanded_training_labels_2"));
-			
+			ClassificationNeuralNetwork<MNISTImage> net = new ClassificationNeuralNetwork<MNISTImage>(
+					new int[] { MNISTImage.PIXEL_COUNT, 150, 10 },
+					ClassificationNeuralNetwork.SIGMOID_ACTIVATION,
+					ClassificationNeuralNetwork.CROSSENTROPY_SIGMOID_COST);
+			//net.SGDScheduledEta(expandedImages, 1, 1.0, 5.0, testingImages, 2, 0.5, 5);
+			//net.SGD(trainingImages, 5, 0.75, 10, 5.0, testingImages);
+			net.SGDScheduledEta(trainingImages, 10, 0.10, 5.0, testingImages, 2, 0.6666666, 3);
+			//net.saveData(new File("expanded_set_150_neurons.ann"));
 		} 
 		catch (Exception e) {
 			e.printStackTrace();
